c10371ea6e0d10be9e7fd210aad3b6a964959724
Mikhail Gorbachev <gor.mihal@gmail.com>
2020-06-13 23:51:31 +0300

    Добавил создание записи расходов

---
+++ src/store/record.js
@@ -0,0 +1,15 @@
+import firabase from 'firebase/app';
+
+export default {
+  actions: {
+    async createRecord({commit, dispatch}, record) {
+      try{
+        const uid = await dispatch('getUid');
+        return await firabase.database().ref(`/users/${uid}/records`).push(record);
+      }catch (e) {
+        commit('setError', e);
+        throw e;
+      }
+    }
+  }
+}
