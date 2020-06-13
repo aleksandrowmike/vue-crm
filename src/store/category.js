69dc4120cdc967509418efbe805f435faf633f3a
Mikhail Gorbachev <gor.mihal@gmail.com>
2020-06-13 22:25:17 +0300

    Добавил создание категорий расходов

---
+++ src/store/category.js
@@ -0,0 +1,16 @@
+import firabase from 'firebase/app';
+
+export default {
+  actions: {
+    async createCategory({commit, dispatch},{title, limit}) {
+      try{
+        const uid = await dispatch('getUid');
+        const category = await firabase.database().ref(`/users/${uid}/categories`).push({title, limit});
+        return {title, limit, id: category.key};
+      }catch (e) {
+        commit('setError', e);
+        throw e;
+      }
+    }
+  }
+}
